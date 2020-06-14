08b93abba9d212c6f59ba193d5b3a92d028fc082
eozkaya5 <eozkaya675@gmail.com>
2020-06-15 01:20:52 +0300

    Model klasörüne  Context sınıfı eklendi.

---
+++ TodoList/Models/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models.Authentication;
+
+namespace TodoList.Models.Context
+{
+    public class AppDbContext: IdentityDbContext<AppUser, AppRole, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContext) : base(dbContext) { }
+    }
+
+}
+
