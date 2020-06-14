84ba093e285cd9eb846ea235536b235c70d74d47
eozkaya5 <eozkaya675@gmail.com>
2020-06-15 01:23:40 +0300

    Models klasörü altındaki VievModels isimli klasöre  ResetPasswordView sınıfı eklendi.

---
+++ TodoList/Models/Viewmodels/ResetPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models.Viewmodels
+{
+    public class ResetPasswordViewModel
+    {
+        [Display(Name = "E-Posta Adresiniz")]
+        [Required(ErrorMessage = "Lütfen e-posta adresinizi boş geçmeyiniz.")]
+        [EmailAddress(ErrorMessage = "Lütfen uygun formatta e-posta giriniz.")]
+        public string Email { get; set; }
+    }
+}
+
