056b9f331b42d715eb8f48d464f9ccfe74063c2a
eozkaya5 <eozkaya675@gmail.com>
2020-06-15 01:25:58 +0300

    Model klasörü altındaki Authentication klasörüne AppRole,AppUser sınıfları oluşturuldu.

---
+++ TodoList/Models/Authentication/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models.Authentication
+{
+    public class AppRole:IdentityRole<int>
+    {
+        public DateTime OluşturmaTarihi { get; set; }
+    }
+}
---
+++ TodoList/Models/Authentication/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models.Authentication
+{
+    public class AppUser:IdentityUser<int>
+    {
+        public string Memleket { get; set; }
+        public bool Cinsiyet { get; set; }
+    }
+}
+
