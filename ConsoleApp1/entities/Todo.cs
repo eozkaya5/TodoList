bf40cd175d7f69f0d2b4d1f8bb9b93d3a7f6abc8
eozkaya5 <eozkaya675@gmail.com>
2020-07-12 23:31:57 +0300

    Liste için model oluşturuldu.

---
+++ ConsoleApp1/entities/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConsoleApp1.entities
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Isim { get; set; }
+        public DateTime OlusturmaTarihi { get; set; }
+        public string Aciklama { get; set; }
+        public int KullaniciId { get; set; }
+    }
+}
